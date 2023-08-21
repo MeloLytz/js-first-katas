3e15f438464cbc3d1ca436b897080cd84871f73e
Melo Lytz <marcelo.lutzenkirchen@gmail.com>
2023-08-21 12:20:10 +0200

    chore: cleans kata structure

--- task-find-the-error/script.js
+++ task-find-the-error/script.js
@@ -72,3 +72,34 @@ console.log(oldYoung(-1));
 console.log(oldYoung(86));
 // result should be elder person
 console.log(oldYoung("Peter"));
+
+
+let userName = "Brad";
+userName = "Jenna";
+
+function getUserNameLength(userName) {
+  return (result = userName.length);
+}
+
+console.log(getUserNameLength(userName) > 4);
+// ^______________ Should log true
+
+console.log("--------------------");
+function isString(string) {
+  return typeof string === "string";
+}
+
+console.log(isString("Hello"));
+// result should be true
+
+console.log(isString(3));
+// result should be false
+
+console.log(isString(undefined));
+// result should be false
+
+console.log(isString(""));
+// result should be true
+
+console.log(isString("John" + "Doe"));
+// result should be true
